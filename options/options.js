122a417a743d59792e906811a0a90221e3643ae5
Crowdbotics <team@crowdbotics.com>
2023-08-04 23:56:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "canal-alliance-42653",
+  url: "https://canal-alliance-42653.botics.co",
+  api: "https://canal-alliance-42653.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
